4761d306ec367dd72e5f13d270a1f8cd3d5cb3fb
batkhishig09 <batkhishig0009@yahoo.com>
2023-11-27 19:29:21 -0600

    Nov27th

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,39 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+    const cleanedStr1 = stringA.replace(/[^\w]/g, '').toLowerCase();
+    const cleanedStr2 = stringB.replace(/[^\w]/g, '').toLowerCase();
+
+    if(cleanedStr1.length !== cleanedStr2.length) {
+        return false;
+    }
+    // characterCounts is created to store the count of each character in cleanedStr1
+    // The for...of loop iterates over each character in cleanedStr1, incrementing its count in the charCount object.
+
+    let characterCounts = {};
+    for (let char of cleanedStr1) {
+        characterCounts[char] = (characterCounts[char] || 0) + 1; 
+    }
+    //This loop iterates over each character in cleanStr2. For each character, it checks if it exists in the charCount object. If it doesn't or its count is already zero, the function returns false. Otherwise, it decreases the count in the charCount object.
+
+    for (let char of cleanedStr2) {
+        if(!characterCounts[char]) {
+            return false;
+        }
+        characterCounts[char] -= 1;
+    }
+    return true
+}
+
+
+// function anagrams(stringA, stringB) {
+//     const cleanedStr1 = stringA.replace(/[^\w]/g, '').toLowerCase();
+//     const cleanedStr2 = stringB.replace(/[^\w]/g, '').toLowerCase();
+
+//     const newString1 = cleanedStr1.split('').sort().join('');
+//     const newString2 = cleanedStr2.split('').sort().join('');
+//     return newString1 === newString2;
+// }
 
 module.exports = anagrams;
