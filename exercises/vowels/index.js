6178119ff3a1864bcdb78bd856d52738d8238fef
batkhishig09 <batkhishig0009@yahoo.com>
2023-12-04 11:58:21 -0600

    dec 4th

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,21 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+    let counter =0;
+    const eachVowels = 'aeoiu'
+    for(char of str.toLowerCase()) {
+        if(eachVowels.includes(char)){
+            counter ++
+        }
+    }
+    return counter;
+ }
+
+//  function vowels(str) {
+//     const matches = str.match(/[aeoiu]/gi);
+//     return matches ? matches.length : 0;
+//  }
+//  console.log(vowels('hello'))
 
 module.exports = vowels;
